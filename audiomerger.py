e939d82e046b8383c2c7b5540928a721ed608bf8
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-04-09 11:15:53 +0530

    Add .venv to .gitignore and update audiomerger.py and test.py

--- audiomerger.py
+++ audiomerger.py
@@ -1,2 +1,9 @@
-from gtts import gTTS
 from pydub import AudioSegment
+
+test = AudioSegment.from_file("intro.mp3")
+test2 = AudioSegment.from_file("test.mp3")
+
+first_10_seconds = test[:10000]
+
+test3 = test2.overlay(test, position=0, gain_during_overlay=10)
+test3.export("test3.mp3", format="mp3")
